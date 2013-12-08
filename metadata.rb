6c3bffdaf3b130a5e3c7a4dc78b651404639ddc5
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-12-08 13:03:24 +0900

    update version

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'ryuzee@gmail.com'
 license          'MIT'
 description      'Installs/Configures apache2'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 depends          "yum"
 
-%w{redhat centos scientific fedora amazon}.each do |os|
+%w{redhat centos scientific fedora amazon ubuntu}.each do |os|
     supports os
 end
